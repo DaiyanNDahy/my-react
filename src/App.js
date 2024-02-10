ac557dac0794c146b24c61c44324a1986ec26a96
DaiyanNDahy <daiyanndahy@gmail.com>
2024-02-10 12:00:19 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -3,22 +3,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <a href="" className="logo">Myblog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
       </header>
-    </div>
+      <div className="Post">
+        <img src="https://techcrunch.com/wp-content/uploads/2024/02/cake-kalk-motorcycle.jpg?w=430&h=230&crop=1"></img>
+        <h2>Motional loses a backer, another micromobility bankruptcy and a mobility startup unicorn is born</h2>
+        <p> The startup, founded and run by Ritu Narayan, raised $140 million in a Series E funding round led by global investment firm GIC. Other investors included Climate Investment, Sequoia and SoftBank Vision Fund 2. The funding pushed its valuation to $1.3 billion
+
+          Zūm has made headway in the past several years, locking in school district customers in San Francisco, Los Angeles, Oakland, Seattle, Chicago, Boston, Nashville and Maryland. Now it’s focused on converting its fleet of vehicles to all EVs by 2027 while continuing to grow its footprint.
+
+          Other deals that got my attention …
+
+          BluSmart, an Indian ride-hailing startup that competes with Uber and homegrown rival Ola, raised $25 million from Switzerland-headquartered impact fund ResponsAbility in a mezzanine structure, including partial equity dilution and debt.
+
+          OK so this isn’t so much a deal, as a dealbreaker. Cake, the Swedish electric motorcycle startup, filed for bankruptcy February 1 after failing to secure enough investment. Cake joins a growing list of micromobility companies that have run into financial trouble over the past year, most recently VanMoof, Superpedestrian and Bird.
+
+          Kura, a UK startup that combined a school bus service with a software platform to safeguard pupils, was acquired by “smart buses” startup Zeelo.
+        </p>
+
+      </div>
+    </main>
   );
 }
 
